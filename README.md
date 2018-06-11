1de223b43bc5cad3339f73215695f3b44f43213d
Gilbert Aragon <gilbertaragon7801@gmail.com>
2018-06-11 02:27:29 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+First React App - MoviePicker
+
+App allows users to find a random movie to watch and/or filter a random movie to watch based on year of release, genre, actor(s)/actress(es), movie runtime, movie rating, and language.
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import MovieDisplay from './MovieDisplay';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MovieDisplay />
+      </div>
+    );
+  }
+}
+
+export default App;
